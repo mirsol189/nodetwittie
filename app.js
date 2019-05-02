92d3fab39ba858a2e6ae337f1215300de992da3b
mirsol189 <dongjun-21@hanmail.net>
2019-05-03 01:50:53 +0900

    code refactoring and bug fixes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
 //connect models with server
-const {sequelize} = require('./models');
+const { sequelize } = require('./models');
 //same as require('./passport/index.js');
 const passportConfig = require('./passport');
 
@@ -25,8 +25,8 @@ passportConfig(passport);
 //view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-//set port 9282
-app.set('port', process.env.PORT || 9282);
+//set port 9284
+app.set('port', process.env.PORT || 9284);
 
 //connect middlewares
 //morgan : show log
@@ -35,18 +35,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/img', express.static(path.join(__dirname, 'uploads')));
 //express built in body-parser : interpret request
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 //cookieParser : interpret cookie in request
 app.use(cookieParser(process.env.COOKIE_SECRET));
 //express-session : manage session
 app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false,
-    },
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
 }));
 //connect-flash : show one-time message
 app.use(flash());
@@ -60,22 +60,21 @@ app.use('/user', userRouter);
 
 //catch 404 and forward to error handler
 app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 //error handler
-app.use((err, req, res) => {
-    //set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    //render the error page
-    res.status(err.status || 500);
-    res.render('error');    
+app.use((err, req, res, next) => {
+  //set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(app.get('port'), () => {
-    console.log('Wating on port ', app.get('port'));
+  console.log('Wating on port ', app.get('port'));
 });
--- models/user.js
+++ models/user.js
@@ -1,25 +1,25 @@
 module.exports = (sequelize, DataTypes) => (
-    sequelize.define('user', {
-        email: {
-            type: DataTypes.STRING(40),
-            allowNull: false,
-            unique: true,
-        },
-        nickname: {
-            type: DataTypes.STRING(15),
-            allowNull: true,
-        },
-        password: {
-            type: DataTypes.STRING(100),
-            allowNull: false,
-        },
-        provider: {
-            type: DataTypes.STRING(10),
-            allowNull: true,
-            defaultValue: 'local',
-        },
-    }, {
-        timestamps: true,
-        paranoid: true,
-    })
+  sequelize.define('user', {
+    email: {
+      type: DataTypes.STRING(40),
+      allowNull: false,
+      unique: true,
+    },
+    nick: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    provider: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+      defaultValue: 'local',
+    },
+  }, {
+    timestamps: true,
+    paranoid: true,
+  })
 );
